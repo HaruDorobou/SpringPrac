2b85fe729c015a5483d521e1110954a3cfdbe375
JSH <suk05088@gmail.com>
2022-07-22 15:54:16 +0900

    spring init

---
+++ src/main/java/com/practice/Hsg/HsgApplication.java
@@ -0,0 +1,13 @@
+package com.practice.Hsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/Hsg/HsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.Hsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
